ea7fac8d4a4b1e7d2364e9d8351015e1c43b4544
Chihau Chau <chihau@gmail.com>
2015-05-15 12:35:37 -0300

    Se agrega primer ejemplo de ios HolaMundo

---
+++ ios/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Chihau Chau on 13-05-15.
+//  Copyright (c) 2015 Chihau Chau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
