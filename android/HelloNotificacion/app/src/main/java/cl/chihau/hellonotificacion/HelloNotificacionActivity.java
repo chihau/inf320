ef773c327ef0b7dc598e7f8246b50b6ab5d920f7
Chihau Chau <chihau@gmail.com>
2016-08-24 06:52:17 -0300

    Actualiza ejemplo HelloNotificacion

--- android/HelloNotificacion/app/src/main/java/cl/chihau/hellonotificacion/HelloNotificacionActivity.java
+++ android/HelloNotificacion/app/src/main/java/cl/chihau/hellonotificacion/HelloNotificacionActivity.java
@@ -1,83 +1,79 @@
 package cl.chihau.hellonotificacion;
 
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-public class HelloNotificacionActivity extends Activity {
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		
-	}
-	
-	public void notificar(View view) {
-		
-		NotificationCompat.Builder mBuilder =
-		        new NotificationCompat.Builder(HelloNotificacionActivity.this)
-		        .setSmallIcon(android.R.drawable.stat_sys_warning)
-		        .setContentTitle("Título de la notificación")
-		        .setContentText("Texto de la notificación")
-		        .setTicker("Alerta!")
-		        .setDefaults(Notification.DEFAULT_ALL)
-		        .setAutoCancel(true); // Para eliminar la notificacion una vez
-									  // que se haya seleccionado
+public class HelloNotificacionActivity extends AppCompatActivity {
 
-		Intent intent0 = new Intent(this, SecondActivity.class);
-		PendingIntent contIntent = PendingIntent.getActivity(
-				this, 0, intent0, PendingIntent.FLAG_UPDATE_CURRENT);
-		mBuilder.setContentIntent(contIntent);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
 
-		Intent intent1 = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("geo:40.8277,-100.8277"));
-		PendingIntent llamar = PendingIntent.getActivity(this, 0, intent1, 0);
-		mBuilder.addAction(android.R.drawable.stat_notify_missed_call, "llamar", llamar);
+    public void notificar(View view) {
 
-		Intent intent2 = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
-		PendingIntent verUrl = PendingIntent.getActivity(this, 0, intent2, 0);
-        mBuilder.addAction(android.R.drawable.stat_notify_more, "ver URL", verUrl);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(android.R.drawable.stat_sys_warning)
+                .setContentTitle("Título de la notificación")
+                .setContentText("Texto de la notificación")
+                .setTicker("Alerta!")
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setAutoCancel(true);
 
-		NotificationManager mNotificationManager =
-		    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent intent1 = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:777777777"));
+        PendingIntent llamar = PendingIntent.getActivity(this, 0, intent1, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.addAction(android.R.drawable.stat_notify_missed_call, "llamar", llamar);
 
-		int mNotificationId = 001;
-		mNotificationManager.notify(mNotificationId, mBuilder.build());
-	}
-	
-	public void dialogo(View view) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        Intent intent2 = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.amazon.com"));
+        PendingIntent verURL = PendingIntent.getActivity(this, 0, intent2, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.addAction(android.R.drawable.stat_notify_more, "ver URL", verURL);
 
-		builder.setMessage("Texto del diálogo")
-		       .setTitle("Título del diálogo");
-		
-		builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
-	           public void onClick(DialogInterface dialog, int id) {
-	               Toast.makeText(getApplicationContext(), Integer.toString(id), Toast.LENGTH_SHORT).show();
-	           }
-	    });
-		builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-	           public void onClick(DialogInterface dialog, int id) {
-	               Toast.makeText(getApplicationContext(), Integer.toString(id), Toast.LENGTH_SHORT).show();
-	           }
-	    });
-        builder.setNeutralButton("Neutral", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                Toast.makeText(getApplicationContext(), Integer.toString(id), Toast.LENGTH_SHORT).show();
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+
+        int mNotificationId = 001;
+
+        notificationManagerCompat.notify(mNotificationId, mBuilder.build());
+    }
+
+    public void dialogo(View view) {
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(this)
+                .setTitle("Título del dialogo")
+                .setMessage("Texto del dialogo");
+
+        mBuilder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int id) {
+                Toast.makeText(HelloNotificacionActivity.this, Integer.toString(id), Toast.LENGTH_SHORT).show();
             }
         });
 
-		AlertDialog dialog = builder.create();
-		dialog.show();
-	}
+        mBuilder.setNeutralButton("Neutral", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int id) {
+                Toast.makeText(HelloNotificacionActivity.this, Integer.toString(id), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mBuilder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int id) {
+                Toast.makeText(HelloNotificacionActivity.this, Integer.toString(id), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        AlertDialog dialog = mBuilder.create();
+        dialog.show();
+    }
 }
