e794581aea43f08bef255e3a50556d3f7b0581e3
Chihau Chau <chihau@gmail.com>
2016-11-22 04:20:11 -0300

    Actualiza ejemplo HelloImageView, ahora soporta swift 3.0.1 y los permisos necesarios para funcionar con iOS 10

--- ios/HelloImageView/HelloImageView/ViewController.swift
+++ ios/HelloImageView/HelloImageView/ViewController.swift
@@ -2,52 +2,55 @@
 //  ViewController.swift
 //  HelloImageView
 //
-//  Created by Chihau Chau on 10-06-15.
-//  Copyright (c) 2015 Chihau Chau. All rights reserved.
+//  Created by Chihau Chau on 22-11-16.
+//  Copyright © 2016 Chihau Chau. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
-    let imagePicker = UIImagePickerController()
+
+    let imagePicker = UIImagePickerController();
     
     @IBOutlet weak var imageView: UIImageView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Seteamos para que nuestro View Controller sea el delegado del UIImagePickerController
         imagePicker.delegate = self
     }
-    
-    // Acción que se ejecuta al presionar el botón
-    @IBAction func abrirPhotoLibrary(sender: AnyObject) {
-        // Deshabilitamos la posibilidad de que el usuario edite la foto
-        imagePicker.allowsEditing = false
-        // Indicamos que la foto la sacaremos la galería
-        imagePicker.sourceType = .PhotoLibrary
-        // Mostramos el Image Picker Controller
-        presentViewController(imagePicker, animated: true, completion: nil)
-    }
 
-    // Método del protocolo UIImagePickerControllerDelegate que le indica al delegado que el usuario ya ha seleccionado una foto
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func openPhotoLibrary(_ sender: Any) {
+        // Deshabilitamos la posibilidad de que el usuario edite la foto
+        imagePicker.allowsEditing = false;
         
-        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
-            // Para que la foto se ajuste al tamaño definido para el ImageView
-            imageView.contentMode = .ScaleAspectFit
-            imageView.image = pickedImage
-        }
+        // Indicamos que la foto la sacaremos la galería, otras opciones del enum son .savedPhotosAlbum y .camera
+        imagePicker.sourceType = .photoLibrary
+        
+        // Mostramos el Image Picker Controller
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    // Método del protocolo UIImagePickerControllerDelegate que le indica al delegado que el usuario ya ha seleccionado una foto
+    func imagePickerController(_ picker: UIImagePickerController,didFinishPickingMediaWithInfo info: [String : Any]) {
+
+        let pickedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        // Para que la foto se ajuste al tamaño definido para el ImageView
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = pickedImage
         
         // Escondemos el Image Picker Controller
-        dismissViewControllerAnimated(true, completion: nil)
+        dismiss(animated: true, completion: nil)
     }
     
     // Método del protocolo UIImagePickerControllerDelegate que le indica al delegado que el usuario canceló la selección
-    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
-        // Escondemos el Image Picker Controller
-        dismissViewControllerAnimated(true, completion: nil)
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 }
-
