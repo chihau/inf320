9bb58c39a96c2ceb95313538c3349f84b4b58d24
Chihau Chau <chihau@gmail.com>
2016-11-22 04:16:07 -0300

    Actualiza ejemplo ToDo, ahora soporta swift 3.0.1

--- ios/ToDo/ToDo/ViewController.swift
+++ ios/ToDo/ToDo/ViewController.swift
@@ -2,91 +2,76 @@
 //  ViewController.swift
 //  ToDo
 //
-//  Created by Chihau Chau on 08-06-15.
-//  Copyright (c) 2015 Chihau Chau. All rights reserved.
+//  Created by Chihau Chau on 22-11-16.
+//  Copyright © 2016 Chihau Chau. All rights reserved.
 //
 
 import UIKit
 import CoreData
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    
     @IBOutlet weak var tableView: UITableView!
     
-    var items: [NSManagedObject] = []
+    //var items = [String]()
+    var items = [NSManagedObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.title = "To Do"
         
         // Se asignamos un identificador a la celda de la tableView para poder reutilizarla
-        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "TableViewCell")
-
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TableViewCell")
     }
 
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Primero debemos obtener el contexto de la aplicación
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        let managedContext = appDelegate.managedObjectContext!
-        
-        // NSFetchRequest es la clase que nos permite obtener los datos desde el CoreData framework
-        let fetchRequest = NSFetchRequest(entityName:"TodoItem")
-        
-        // Creamos una variable que puede guardar errores
-        var error: NSError?
-        
-        // Obtenemos los datos
-        let fetchedResults = managedContext.executeFetchRequest(fetchRequest, error: &error) as? [NSManagedObject]
-        
-        // Si no hay datos imprimimos un error
-        if let results = fetchedResults {
-            items = results
-        } else {
-            println("Could not fetch \(error), \(error!.userInfo)")
-        }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
-
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.items.count
     }
     
     // Poblamos el tableView con los datos obtenidos desde la BD
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let item = self.items[indexPath.row]
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         // Reutilizamos la celda con el identificador "TableViewCell"
-        let tableViewCell = tableView.dequeueReusableCellWithIdentifier("TableViewCell", forIndexPath: indexPath) as! UITableViewCell
+        let tableViewCell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath as IndexPath)
         
         // Obtenemos el valor del campo "desc"
-        tableViewCell.textLabel?.text = item.valueForKey("desc") as? String
+        //tableViewCell.textLabel!.text = items[indexPath.row]
+        tableViewCell.textLabel!.text = items[indexPath.row].value(forKey: "texto") as? String
         
         return tableViewCell
     }
-
-    // Acción que ejecuta el botón "Add" 
-    @IBAction func addName(sender: AnyObject) {
-        var alert = UIAlertController(title: "Nueva Tarea", message: "Agregar una nueva tarea", preferredStyle: .Alert)
+    
+    // Acción que ejecuta el botón "Add"
+    @IBAction func addItem(_ sender: Any) {
+        let alert = UIAlertController(title: "Nueva Tarea", message: "Agregar una nueva tarea", preferredStyle: .alert)
         
         // Definimos la acción "Guardar"
-        let saveAction = UIAlertAction(title: "Guardar", style: .Default) { (action: UIAlertAction!) -> Void in
-                
-                let textField = alert.textFields![0] as! UITextField
-                self.saveName(textField.text)
-                self.tableView.reloadData()
+        let saveAction = UIAlertAction(title: "Guardar", style: .default) { (action: UIAlertAction!) -> Void in
+            
+            let textField = alert.textFields![0]
+            
+            //self.items.append(textField.text!)
+            self.saveItem(texto: textField.text!)
+            
+            self.tableView.reloadData()
         }
         
         // Definimos la acción "Cancelar"
-        let cancelAction = UIAlertAction(title: "Cancelar", style: .Default) { (action: UIAlertAction!) -> Void in
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .default) { (action: UIAlertAction!) -> Void in
             
         }
         
         // Agregamos el campo de texto de la alerta
-        alert.addTextFieldWithConfigurationHandler {
+        alert.addTextField {
             (textField: UITextField!) -> Void in
-            //Aquí podrimos configurar el campo de texto antes de ser mostrado
+            //Aquí podríamos configurar el campo de texto antes de ser mostrado
         }
         
         // Agregamos los botones de la alerta
@@ -94,30 +79,50 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         alert.addAction(cancelAction)
         
         // Mostramos la ventana de Alerta
-        presentViewController(alert, animated: true, completion: nil)
+        present(alert, animated: true, completion: nil)
     }
     
     // Método para agregar datos a la BD
-    func saveName(txt: String) {
+    func saveItem(texto: String) {
         // Primero debemos obtener el contexto de la aplicación
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        let managedContext = appDelegate.managedObjectContext!
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
         
         // Obtenemos una instancia de la entidad (tabla) de la BD
-        let entity =  NSEntityDescription.entityForName("TodoItem", inManagedObjectContext: managedContext)
-        let desc = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        let entity =  NSEntityDescription.entity(forEntityName: "Item", in: managedContext)
+        let attribute = NSManagedObject(entity: entity!, insertInto: managedContext)
         
         // Insertamos el nuevo valor en el campo "desc" de la table "TodoItem"
-        desc.setValue(txt, forKey: "desc")
+        attribute.setValue(texto, forKey: "texto")
         
         // Guardamos el cambio en la BD, sino se puede imprimimos un error
-        var error: NSError?
-        if !managedContext.save(&error) {
-            println("Could not save \(error), \(error?.userInfo)")
-        }  
-        
-        // Agregamos un item nuevo a la tableView
-        items.append(desc)
+        do {
+            try managedContext.save()
+            items.append(attribute)
+        } catch let error as NSError  {
+            print("No se puede guardar el item \(error), \(error.userInfo)")
+        }
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Primero debemos obtener el contexto de la aplicación
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // NSFetchRequest es la clase que nos permite obtener los datos desde el CoreData framework
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Item")
+        
+        // Obtenemos los datos
+        do {
+            let results =
+                try managedContext.fetch(fetchRequest)
+            items = results as! [NSManagedObject]
+        } catch let error as NSError {
+            print("No se pueden obtener los items guardados \(error), \(error.userInfo)")
+        }
+    }
+    
 }
 
