7ed086795bedd69b117a1cf9fe6a4fb54d91329b
Chihau Chau <chihau@gmail.com>
2018-11-27 01:46:35 -0300

    Actualiza ejemplo HelloWorld

--- ios/HelloWorld/HelloWorld/ViewController.swift
+++ ios/HelloWorld/HelloWorld/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  HelloWorld
 //
-//  Created by Chihau Chau on 03-11-16.
-//  Copyright © 2016 Chihau Chau. All rights reserved.
+//  Created by Chihau Chau on 27-11-18.
+//  Copyright © 2018 Chihau Chau. All rights reserved.
 //
 
 import UIKit
@@ -13,11 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
