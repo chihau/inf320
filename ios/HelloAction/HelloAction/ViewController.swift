266456999825c76901fc9f21fd248e930dcf410a
Chihau Chau <chihau@gmail.com>
2018-12-06 01:46:41 -0300

    Actualiza ejemplo HelloAction

--- ios/HelloAction/HelloAction/ViewController.swift
+++ ios/HelloAction/HelloAction/ViewController.swift
@@ -2,39 +2,26 @@
 //  ViewController.swift
 //  HelloAction
 //
-//  Created by Chihau Chau on 01-06-15.
-//  Copyright (c) 2015 Chihau Chau. All rights reserved.
+//  Created by Chihau Chau on 04-12-18.
+//  Copyright © 2018 Chihau Chau. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet weak var myTextField: UITextField!
-    
     @IBOutlet weak var myLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        myTextField.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func ingresarTexto(sender: AnyObject) {
+    @IBAction func ingresarTexto(_ sender: Any) {
         myLabel.text = myTextField.text
         myTextField.resignFirstResponder()
     }
     
-    func textFieldShouldReturn(userText: UITextField) -> Bool {
-        myLabel.text = myTextField.text
-        userText.resignFirstResponder()
-        return true;
-    }
-
 }
 
