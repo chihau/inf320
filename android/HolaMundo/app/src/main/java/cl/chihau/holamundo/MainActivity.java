dd35a855b3c94f83ffc57124d1285fc1288172a5
Chihau Chau <chihau@gmail.com>
2019-10-02 13:16:04 -0300

    Actualiza ejemplo HolaMundo

--- android/HolaMundo/app/src/main/java/cl/chihau/holamundo/MainActivity.java
+++ android/HolaMundo/app/src/main/java/cl/chihau/holamundo/MainActivity.java
@@ -1,14 +1,14 @@
 package cl.chihau.holamundo;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 }
