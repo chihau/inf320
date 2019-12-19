eb32540d9bb5425e1aab2d0dbfdeed02c78f8be6
Chihau Chau <chihau@gmail.com>
2019-12-19 11:21:26 -0300

    Actualiza ejemplo MisPaises

--- android/MisPaises/app/src/main/java/cl/chihau/mispaises/MisPaisesActivity.java
+++ android/MisPaises/app/src/main/java/cl/chihau/mispaises/MisPaisesActivity.java
@@ -1,97 +1,101 @@
 package cl.chihau.mispaises;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class MisPaisesActivity extends Activity implements OnItemClickListener, OnClickListener {
-	
-	List<String> countries = new ArrayList<String>();
-	ArrayAdapter<String> adapter;
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		setContentView(R.layout.main);
-		
-		ListView lv = (ListView) findViewById(R.id.mylistview);
-		
-		loadData();
-		
-		adapter = new ArrayAdapter<String>(this, R.layout.list_item, countries);
-		
-		lv.setAdapter(adapter);
-		
-		lv.setOnItemClickListener(this);
-		
-		Button btn = (Button) findViewById(R.id.mybutton);
-		btn.setOnClickListener(this);
-	}
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
-		Uri uri = Uri.parse("http://en.wikipedia.org/wiki/" + Uri.encode(countries.get(pos)));
-		
-		Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-		startActivity(intent);
-	}
+public class MisPaisesActivity extends AppCompatActivity implements View.OnClickListener,
+        AdapterView.OnItemClickListener {
 
-	@Override
-	public void onClick(View v) {
-		EditText et = (EditText) findViewById(R.id.mytextview);
-		
-		if (et.getText().toString().length() > 0) {
-			if (countries.contains(et.getText().toString())) {
-				Toast.makeText(this, "Este país ya existe en la lista", Toast.LENGTH_LONG).show();
-			} else {
-				String country = et.getText().toString();
-				countries.add(country.substring(0, 1).toUpperCase() + 
-						country.substring(1, country.length()).toLowerCase());
-				Collections.sort(countries);
-				adapter.notifyDataSetChanged();
-				et.setText("");
-				saveData();
-			}
-		} else {
-			Toast.makeText(this, "El campo está vacío", Toast.LENGTH_LONG).show();
-		}
-	}
+    List countries = new ArrayList();
+    ArrayAdapter adapter;
 
-	private void loadData() {
-		SharedPreferences sp = getPreferences(MODE_PRIVATE);
-		String countryList = sp.getString("countries", "Argentina;Brasil;Bolivia;Chile;Perú");
-		
-		for (String country : countryList.split(";")) {
-			countries.add(country);
-		}
-	}
-	
-	private void saveData() {
-		SharedPreferences.Editor spe = getPreferences(MODE_PRIVATE).edit();
-		
-		StringBuilder sb = new StringBuilder();
-		int i;
-		for (i = 0; i < countries.size(); i++) {
-			sb.append( ((i == 0) ? "" : ";")  + countries.get(i));
-		}
-		
-		spe.putString("countries", sb.toString());
-		spe.commit();
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        ListView lv = (ListView) findViewById(R.id.mylistview);
+
+        loadData();
+
+        adapter = new ArrayAdapter(this, R.layout.list_item, countries);
+
+        lv.setAdapter(adapter);
+
+        Button btn = (Button) findViewById(R.id.mybutton);
+        btn.setOnClickListener(this);
+
+        lv.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        EditText et = (EditText) findViewById(R.id.myedittext);
+
+        String country = et.getText().toString();
+
+        country = country.substring(0, 1).toUpperCase() +
+                country.substring(1, country.length()).toLowerCase();
+
+        if (country.length() > 0) {
+            if (countries.contains(country)) {
+                Toast.makeText(this, "Este país ya existe en la lista", Toast.LENGTH_SHORT).show();
+            } else {
+                countries.add(country);
+                Collections.sort(countries);
+                adapter.notifyDataSetChanged();
+                et.setText("");
+                saveData();
+            }
+        } else {
+            Toast.makeText(this, "El campo está vacío", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
+        Uri uri = Uri.parse("http://en.wikipedia.org/wiki/" +
+                Uri.encode(countries.get(pos).toString()));
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    private void saveData() {
+        SharedPreferences.Editor spe = getPreferences(MODE_PRIVATE).edit();
+
+        StringBuilder sb = new StringBuilder();
+        int i;
+        for (i = 0; i < countries.size(); i++) {
+            sb.append( ((i == 0) ? "" : ";") + countries.get(i));
+        }
+
+        spe.putString("countries", sb.toString());
+        spe.commit();
+    }
+
+    private void loadData() {
+        SharedPreferences sp = getPreferences(MODE_PRIVATE);
+        String countryList = sp.getString("countries",
+                "Argentina;Brasil;Bolivia;Chile;Perú");
+
+        for (String country : countryList.split(";")) {
+            countries.add(country);
+        }
+    }
 }
