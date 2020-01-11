68a438ab78e669bdd8c7d94b4d8c09f2cc801205
Chihau Chau <chihau@gmail.com>
2020-01-11 14:21:02 -0300

    Actualiza ejemplo HolaReceivers

--- android/HolaReceivers/app/src/main/java/cl/chihau/holareceivers/MainActivity.java
+++ android/HolaReceivers/app/src/main/java/cl/chihau/holareceivers/MainActivity.java
@@ -9,38 +9,36 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.BatteryManager;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends ActionBarActivity {
-    private static final String TAG = "HolaReceivers";
+public class MainActivity extends AppCompatActivity {
 
-    private TextView tvNetwork;
-    private TextView tvBattery;
+    TextView tvNetwork;
+    TextView tvBattery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvNetwork = (TextView) findViewById(R.id.tv_network);
-        tvBattery = (TextView) findViewById(R.id.tv_battery);
+        tvNetwork = findViewById(R.id.tv_network);
+        tvBattery = findViewById(R.id.tv_battery);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-
-        registerReceivers(true);
+        registrarReceivers(true);
     }
 
     @Override
     public void onPause() {
         super.onPause();
 
-        registerReceivers(false);
+        registrarReceivers(false);
     }
 
     private BroadcastReceiver netStatusReceiver = new BroadcastReceiver() {
@@ -57,57 +55,49 @@ public class MainActivity extends ActionBarActivity {
         }
     };
 
-    private void registerReceivers(boolean encender) {
+    private void registrarReceivers(boolean encender) {
         if (encender) {
-            registerReceiver(netStatusReceiver, new IntentFilter(
-                    ConnectivityManager.CONNECTIVITY_ACTION));
-
-            // Intent.ACTION_BATTERY_CHANGED es un sticky intent
-            registerReceiver(batteryStatusReceiver, new IntentFilter(
-                    Intent.ACTION_BATTERY_CHANGED));
-            Log.d(TAG, "Broadcast Receivers ON");
+            registerReceiver(netStatusReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+            registerReceiver(batteryStatusReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+            Log.d("HolaReceivers", "Broadcast Receivers ON");
         } else {
             unregisterReceiver(netStatusReceiver);
-            unregisterReceiver(batteryStatusReceiver);
-            Log.d(TAG, "Broadcast Receivers OFF");
+            Log.d("HolaReceivers", "Broadcast Receivers OFF");
         }
     }
 
     private void checkNetStatus() {
-        ConnectivityManager cm = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
-
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        boolean isConnected = activeNetwork != null
-                && activeNetwork.isConnectedOrConnecting();
+
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
 
         if (isConnected) {
-            if (activeNetwork.getTypeName().equals("WIFI")) {
-                tvNetwork.setText("Estado de la Red: conectado por Red Wifi");
-                //Hay teléfonos que devuelven "mobile" en mayúsculas y en minúsculas
+            if (activeNetwork.getTypeName().equals("WIFI")){
+                tvNetwork.setText("Estado de la Red: Conectado por Red Wifi");
             } else if (activeNetwork.getTypeName().toUpperCase().equals("MOBILE")) {
                 tvNetwork.setText("Estado de la Red: Conectado por Red Móvil");
             }
             tvNetwork.setTextColor(Color.BLACK);
         } else {
-            tvNetwork.setText("Estado de la Red: desconectado");
+            tvNetwork.setText("Estado de la Red: Desconectado");
             tvNetwork.setTextColor(Color.RED);
         }
-
     }
 
     private void checkBatteryStatus(Intent intent) {
         int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+
         boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
                 status == BatteryManager.BATTERY_STATUS_FULL;
 
         int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
 
         if (isCharging) {
-            tvBattery.setText("Estado de la Batería: cargando con " + level + "%");
+            tvBattery.setText("Estado de la Batería: Cargando con " + level + "%");
             tvBattery.setTextColor(Color.BLACK);
         } else {
-            tvBattery.setText("Estado de la Batería: descargando con " + level + "%");
+            tvBattery.setText("Estado de la Batería: Descargando con " + level + "%");
             tvBattery.setTextColor(Color.RED);
         }
     }
