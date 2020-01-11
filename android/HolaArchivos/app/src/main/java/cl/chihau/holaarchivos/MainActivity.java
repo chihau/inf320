161ebe8ec58b954ed9c74b965f4c2fc4f8e268fc
Chihau Chau <chihau@gmail.com>
2020-01-11 14:14:46 -0300

    Actualiza ejemplo HolaArchivos

--- android/HolaArchivos/app/src/main/java/cl/chihau/holaarchivos/MainActivity.java
+++ android/HolaArchivos/app/src/main/java/cl/chihau/holaarchivos/MainActivity.java
@@ -5,9 +5,6 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +18,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 public class MainActivity extends AppCompatActivity {
     TextView txtResultado;
     Button btnEscribirSD;
@@ -30,22 +31,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtResultado = findViewById(R.id.txtResultado);
+
+        txtResultado = findViewById(R.id.txt_resultado);
         btnEscribirSD = findViewById(R.id.btn_escribir_sd);
         btnLeerSD = findViewById(R.id.btn_leer_sd);
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
 
-        if (checkPermission()) {
+        if (chequearPermiso()) {
             btnEscribirSD.setEnabled(true);
             btnLeerSD.setEnabled(true);
         } else {
             btnEscribirSD.setEnabled(false);
             btnLeerSD.setEnabled(false);
-            requestPermission();
+            pedirPermiso();
         }
     }
 
@@ -58,32 +60,32 @@ public class MainActivity extends AppCompatActivity {
             txtResultado.setText("Archivo creado!");
         } catch (Exception e) {
             Log.e("HolaArchivos", "Error al escribir el archivo en memoria interna");
+            txtResultado.setText("Erro al escribir el archivo en memoria interna");
         }
     }
 
     public void leerArchivo(View view) {
         try {
-            BufferedReader fin = new BufferedReader(new InputStreamReader(
-                    openFileInput("prueba_int.txt")));
+            BufferedReader fin = new BufferedReader(new InputStreamReader(openFileInput("prueba_int.txt")));
             String texto = fin.readLine();
             fin.close();
             txtResultado.setText(texto);
         } catch (Exception e) {
-            Log.e("HolaArchivo", "Error al leer el archivo en memoria interna");
+            Log.e("HolaArchivos", "Error al leer el archivo en memoria interna");
+            txtResultado.setText("Error al leer el archivo en memoria interna");
         }
     }
 
     public void escribirSD(View view) {
-
         boolean sdDisponible = false;
         boolean sdAccesoEscritura = false;
 
         String estado = Environment.getExternalStorageState();
 
-        if (estado.equals(Environment.MEDIA_MOUNTED)) {
+        if(estado.equals(Environment.MEDIA_MOUNTED)) {
             sdDisponible = true;
             sdAccesoEscritura = true;
-        } else if (estado.equals(Environment.MEDIA_MOUNTED_READ_ONLY)) {
+        } else if(estado.equals(Environment.MEDIA_MOUNTED_READ_ONLY)) {
             sdDisponible = true;
             sdAccesoEscritura = false;
         } else {
@@ -95,35 +97,36 @@ public class MainActivity extends AppCompatActivity {
             try {
                 File ruta_sd = Environment.getExternalStorageDirectory();
                 File f = new File(ruta_sd.getAbsolutePath(), "prueba_sd.txt");
-                OutputStreamWriter fout = new OutputStreamWriter(
-                        new FileOutputStream(f));
+                OutputStreamWriter fout = new OutputStreamWriter(new FileOutputStream(f));
                 fout.write("Texto de prueba desde SD");
                 fout.close();
-
                 txtResultado.setText("Archivo SD creado!");
             } catch (Exception ex) {
-                Log.e("HolaArchivos", "Error al escribir el archivo en memoria SD");
-                Log.e("HolaArchivos", ex.getMessage());
+                Log.e("HolaArchivo", "Error al escribir el archivo en memoria externa");
+                txtResultado.setText("Error al escribir el archivo en memoria externa");
             }
         } else {
-            Log.e("HolaArchivos", "Error: la tarjeta SD no se encuentra " +
-                    "o no tiene permisos de escritura");
+            Log.e("HolaArchivos", "Error: la tarjeta SD no se encuentra o no tiene permisos" +
+                    "de escritura");
+            txtResultado.setText("Error: la tarjeta SD no se encuentra o no tiene permisos" +
+                    "de escritura");
         }
+
     }
 
     public void leerSD(View view) {
         try {
             File ruta_sd = Environment.getExternalStorageDirectory();
             File f = new File(ruta_sd.getAbsolutePath(), "prueba_sd.txt");
-
-            BufferedReader fin = new BufferedReader(new InputStreamReader(
-                    new FileInputStream(f)));
+            BufferedReader fin = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
             String texto = fin.readLine();
             fin.close();
-
             txtResultado.setText(texto);
-        } catch (Exception ex) {
-            Log.e("HolaArchivos", "Error al leer el archivo desde la memoria SD");
+        } catch (Exception e) {
+            Log.e("HolaArchivos", "Error: la tarjeta SD no se encuentra o no tiene permisos" +
+                    "de lectura");
+            txtResultado.setText("Error: la tarjeta SD no se encuentra o no tiene permisos" +
+                    "de lectura");
         }
     }
 
@@ -133,41 +136,34 @@ public class MainActivity extends AppCompatActivity {
             BufferedReader brin = new BufferedReader(new InputStreamReader(fraw));
             String texto = brin.readLine();
             fraw.close();
+            brin.close();
             txtResultado.setText(texto);
-        } catch (Exception ex) {
-            Log.e("HolaArchivos", "Error al leer el archivo desde recurso raw");
+        } catch (Exception e) {
+            Log.e("HolaArchivos", "Error al leer el archivo RAW");
+            txtResultado.setText("Error al leer el archivo RAW");
         }
     }
 
-    private boolean checkPermission() {
-        int result = ContextCompat.checkSelfPermission(getApplicationContext(),
-                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+    private boolean chequearPermiso() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
         return result == PackageManager.PERMISSION_GRANTED;
     }
 
-    private void requestPermission() {
-        ActivityCompat.requestPermissions(this,
-                new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},
+    private void pedirPermiso() {
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},
                 200);
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 200: {
-                // Si el request es cancelado entonces el arreglo tiene largo 0
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    btnEscribirSD.setEnabled(true);
-                    btnLeerSD.setEnabled(true);
-                } else {
-                    btnEscribirSD.setEnabled(false);
-                    btnLeerSD.setEnabled(false);
-                }
-
-                return;
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == 200) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                btnEscribirSD.setEnabled(true);
+                btnLeerSD.setEnabled(true);
+            } else {
+                btnEscribirSD.setEnabled(false);
+                btnLeerSD.setEnabled(false);
             }
         }
     }
