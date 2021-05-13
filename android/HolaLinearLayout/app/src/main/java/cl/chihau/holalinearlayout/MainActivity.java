546c7bdf9f938f7a946874a7e6944c674294f1e4
Chihau Chau <chihau@gmail.com>
2021-05-13 10:54:59 -0400

    Actualiza ejemplo HolaLinearLayout

--- android/HolaLinearLayout/app/src/main/java/cl/chihau/holalinearlayout/MainActivity.java
+++ android/HolaLinearLayout/app/src/main/java/cl/chihau/holalinearlayout/MainActivity.java
@@ -1,8 +1,8 @@
 package cl.chihau.holalinearlayout;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
